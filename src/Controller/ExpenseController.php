6b3bb0762607d6267e48153fcc0f31d576095b73
Soufiane Aït <soufiane@dbep.fr>
2022-11-01 20:11:28 +0100

    setup post api

--- src/Controller/ExpenseController.php
+++ src/Controller/ExpenseController.php
@@ -2,42 +2,58 @@
 
 namespace App\Controller;
 
+use App\Entity\Budget;
 use App\Entity\Expense;
 use App\Form\ExpenseType;
 use App\Repository\ExpenseRepository;
+use DateTimeImmutable;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\IsNull;
+use Symfony\Component\Validator\Constraints\NotNull;
 
-#[Route('/expense')]
+#[Route('api/expense')]
 class ExpenseController extends AbstractController
 {
     #[Route('/', name: 'app_expense_index', methods: ['GET'])]
-    public function index(ExpenseRepository $expenseRepository): Response
+    public function index(Request $request, ExpenseRepository $expenseRepository, LoggerInterface $logger): Response
     {
+        $logger->error('here are the budgets bro');
         return $this->render('expense/index.html.twig', [
             'expenses' => $expenseRepository->findAll(),
         ]);
     }
-
-    #[Route('/new', name: 'app_expense_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, ExpenseRepository $expenseRepository): Response
+    // Je dois supprimer methods[POST] pour ne plus avoir l'erreur GET ????!!!
+    #[Route('/add/{id}', name: 'app_expense_new')]
+    public function new(Request $request, ExpenseRepository $expenseRepository, Budget $budget): Response
     {
-        $expense = new Expense();
-        $form = $this->createForm(ExpenseType::class, $expense);
-        $form->handleRequest($request);
+        // $request->header('Access-Control-Allow-ORigin', 1, 200);
+        $data = json_decode($request->getContent(), true);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $expenseRepository->save($expense, true);
-
-            return $this->redirectToRoute('app_expense_index', [], Response::HTTP_SEE_OTHER);
+        if (null == $data) {
+            return $this->json([
+                'success' => false,
+                'message' => 'Aucun paramètre POST trouvé'], 300);
         }
 
-        return $this->renderForm('expense/new.html.twig', [
-            'expense' => $expense,
-            'form' => $form,
-        ]);
+        // Building the date
+        $date = new DateTimeImmutable();
+        $date::createFromFormat('Y-m-d', $data['expense_date']);
+
+        // return $this->json($data);
+        $expense = new Expense();
+        $expense->setAmount($data['expense_amount']);
+        // dump($data);:
+        $expense->setDate($date);
+        $expense->setDescription($data['expense_description']);
+        $expense->setBudget($budget);
+        $expenseRepository->save($expense, true);
+
+        return $this->json($expense);
     }
 
     #[Route('/{id}', name: 'app_expense_show', methods: ['GET'])]
