aa351d32515e95086c8f74120aa27e8ab0f15dd4
Soufiane Aït <soufiane@dbep.fr>
2022-11-01 20:10:59 +0100

    monolog setup

--- config/bundles.php
+++ config/bundles.php
@@ -10,5 +10,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
